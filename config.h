314d90002c60a9825ceead4284ece6948098affd
Raveendra Soori <sooriravindra@gmail.com>
2019-07-30 00:06:45 -0400

    Use unicode chars for layout indication and dark bg for the window title in status bar

--- config.h
+++ config.h
@@ -39,8 +39,8 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[┼]",      tile },    /* first entry is default */
+	{ "[▚]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
--- dwm.c
+++ dwm.c
@@ -730,7 +730,7 @@ drawbar(Monitor *m)
 
 	if ((w = m->ww - sw - x) > bh) {
 		if (m->sel) {
-			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
+			drw_setscheme(drw, scheme[m == selmon ? SchemeNorm : SchemeNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
