1d24e19855efe177ae57c8e1cf8cce04bcb84081
Raveendra Soori <sooriravindra@gmail.com>
2019-08-05 22:44:47 -0400

    Next and prev go to only tags with clients

--- dwm.c
+++ dwm.c
@@ -2210,18 +2210,34 @@ zoom(const Arg *arg)
 }
 
 void
-shiftview(const Arg *arg) {
-	Arg shifted;
+shiftview(const Arg *arg)
+{
+	Arg a;
+	Monitor *m;
+	Client *c;
+	unsigned visible = 0;
+	int i = arg->i;
+	int nextseltags, curseltags = selmon->tagset[selmon->seltags];
 
-	if(arg->i > 0) // left circular shift
-		shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
-		   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
+	do {
+		if(i > 0) // left circular shift
+			nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
 
-	else // right circular shift
-		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
-		   | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+		else // right circular shift
+			nextseltags = curseltags >> (- i) | (curseltags << (LENGTH(tags) + i));
 
-	view(&shifted);
+                // Check if tag is visible
+		for (m = mons; m && !visible; m = m->next)
+			for (c = m->clients; c; c = c->next)
+				if (nextseltags & c->tags) {
+                                    visible = 1;
+                                    break;
+                                }
+		i += arg->i;
+	} while (!visible);
+
+	a.i = nextseltags;
+	view(&a);
 }
 
 int
