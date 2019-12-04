07560d0341ada89be7e5542a6d95f28fa51ebda4
Raveendra Soori <sooriravindra@gmail.com>
2019-12-04 13:39:46 -0500

    Use fontawesome in the bar

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "monospace:size=14","FontAwesome:size=14",};
+static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -39,9 +39,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[┼]",      tile },    /* first entry is default */
-	{ "[▚]",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "\uF009",      tile },    /* first entry is default */
+	{ "\uF2D2",      NULL },    /* no layout function means floating behavior */
+	{ "\uF065",      monocle },
 };
 
 /* key definitions */
